4f54aa7320aa8f1bb9cfc7a4cd027c084add9b47
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 19:24:55 +0700

    List Products - Show List Products in Activity

--- app/src/main/java/id/belanja/app/MainActivity.kt
+++ app/src/main/java/id/belanja/app/MainActivity.kt
@@ -2,11 +2,29 @@ package id.belanja.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import id.belanja.app.adapter.ListProductAdapter
+import id.belanja.app.model.Products
+import id.belanja.app.model.ProductsData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var list: ArrayList<Products> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showListProducts()
+    }
+
+    private fun showListProducts() {
+        list.addAll(ProductsData.listProduct)
+        rvProducts.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ListProductAdapter(list)
+            setHasFixedSize(true)
+        }
     }
 }
