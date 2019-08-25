ac4c79658fa354939a7d51c8718dd0598805afa1
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 20:15:40 +0700

    List Products - Setup Add Product Action

--- app/src/main/java/id/belanja/app/MainActivity.kt
+++ app/src/main/java/id/belanja/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.belanja.app
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         showListProducts()
+        setupAddProduct()
     }
 
     private fun showListProducts() {
@@ -25,6 +27,13 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = ListProductAdapter(list)
             setHasFixedSize(true)
+        }
+    }
+
+    private fun setupAddProduct() {
+        btnAddProduct.setOnClickListener {
+            val detailIntent = Intent(this, DetailProductActivity::class.java)
+            startActivity(detailIntent)
         }
     }
 }
