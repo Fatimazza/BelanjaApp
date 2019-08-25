9ac16520db761a15610a9ea9dc6d0ed328e9116c
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 20:58:55 +0700

    Intent List Products - Receive Intent of Detail Product

--- app/src/main/java/id/belanja/app/DetailProductActivity.kt
+++ app/src/main/java/id/belanja/app/DetailProductActivity.kt
@@ -2,6 +2,8 @@ package id.belanja.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detail_product.*
 
 class DetailProductActivity : AppCompatActivity() {
 
@@ -14,5 +16,16 @@ class DetailProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
+
+        showProductDetail()
+    }
+
+    private fun showProductDetail() {
+        etProductNameDetail.setText(intent.getStringExtra(EXTRA_NAME))
+        etProductPriceDetail.setText(intent.getStringExtra(EXTRA_PRICE))
+        etProductImageDetail.setText(intent.getStringExtra(EXTRA_IMAGE_URL))
+
+        Glide.with(this)
+            .load(intent.getStringExtra(EXTRA_IMAGE_URL)).into(imgProductDetail)
     }
 }
