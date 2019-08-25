7ad3cef0b468bedc985907c9c64f6605ec815b5d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 18:55:18 +0700

    List Products - Add Initial Adapter

---
+++ app/src/main/java/id/belanja/app/adapter/ListProductAdapter.kt
@@ -0,0 +1,24 @@
+package id.belanja.app.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class ListProductAdapter : RecyclerView.Adapter<ListProductAdapter.ListProductViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListProductAdapter.ListProductViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ListProductAdapter.ListProductViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ListProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
