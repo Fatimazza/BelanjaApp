cbe12d27b05fa850b0cd91969bdb12bd4699c68f
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 20:40:39 +0700

    OnClick List Products - Set onClick Action in List Activity

--- app/src/main/java/id/belanja/app/MainActivity.kt
+++ app/src/main/java/id/belanja/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import id.belanja.app.adapter.ListProductAdapter
 import id.belanja.app.model.Products
 import id.belanja.app.model.ProductsData
@@ -13,19 +14,24 @@ class MainActivity : AppCompatActivity() {
 
     private var list: ArrayList<Products> = arrayListOf()
 
+    private lateinit var listProductAdapter: ListProductAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showListProducts()
         setupAddProduct()
+        setListClickAction()
     }
 
     private fun showListProducts() {
         list.addAll(ProductsData.listProduct)
+        listProductAdapter = ListProductAdapter(list)
+
         rvProducts.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = ListProductAdapter(list)
+            adapter = listProductAdapter
             setHasFixedSize(true)
         }
     }
@@ -35,5 +41,13 @@ class MainActivity : AppCompatActivity() {
             val detailIntent = Intent(this, DetailProductActivity::class.java)
             startActivity(detailIntent)
         }
+    }
+
+    private fun setListClickAction() {
+        listProductAdapter.setOnItemClickCallback(object : ListProductAdapter.OnItemClickCallback {
+            override fun onItemClick(data: Products) {
+                Toast.makeText(this@MainActivity, "Anda memilih ${data.name}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
