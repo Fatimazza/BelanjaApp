1827e1db3f31b1fabb94b4a585e6b01e0ad166dd
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 19:39:39 +0700

    List Products - Show Image in List Products

--- app/src/main/java/id/belanja/app/adapter/ListProductAdapter.kt
+++ app/src/main/java/id/belanja/app/adapter/ListProductAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import id.belanja.app.R
 import id.belanja.app.model.Products
 import kotlinx.android.synthetic.main.item_list_product.view.*
@@ -28,6 +29,7 @@ class ListProductAdapter(val listProduct: ArrayList<Products>) :
             with(itemView) {
                 tvProductName.text = products.name
                 tvProductPrice.text = products.price.toString()
+                Glide.with(this).load(products.image).into(imgProduct)
             }
         }
     }
