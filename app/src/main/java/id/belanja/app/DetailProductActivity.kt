dcf6a4f0d629e5954f190daacbf321e8c58760db
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-25 19:30:22 +0700

    Detail Product - Add new Activity & Layout

---
+++ app/src/main/java/id/belanja/app/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package id.belanja.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
